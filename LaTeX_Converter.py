b2b05f674238b13517a27557cf79c0a0ad56e436
Kyle Begovich <begovichkyle@gmail.com>
2017-11-04 23:43:26 -0500

    next 3 hour progress

---
+++ LaTeX_Converter.py
@@ -0,0 +1,133 @@
+import sys
+import os
+import time
+
+
+SQUIGGLES = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+RUNNING_CONVERSION_MESSAGE = "\nRunning conversion script!\n" + SQUIGGLES + "\n"
+SUCCESS_MESSAGE = "Success! Saving..."
+BAD_FILE_PATH_MESSAGE = " ~ skipping bad file path ~\n"
+END_DOC_STR = "\n\end{document}"
+MAKING_OUT_DIR_MESSAGE = "Creating output directory: LaTeX_Outputs/\n\n"
+IOERROR_MESSAGE = "Unable to read file under path, or file does not exist.\nIOError for path: "
+NOT_ENOUGH_ARGS_ERROR_MESSAGE = "Not enough arguments!\n    usage: python3 LaTeX_Converter.py <file_path> [files ...]"
+BASE_TEXT_NOT_FOUND_ERROR_MESSAGE = "base_text.tex file not found!\nPlease do one of the following:" \
+                "\n    Unix: $ curl http://www.kylebegovich.com/wp-content/uploads/2017/11/LaTeX.txt -o base_text.tex" \
+                "\n    otherwise: re-install from GitHub"
+COMPLETED_MESSAGE = "\nFinished conversion! Find your files with the .tex extension:\n" + SQUIGGLES
+OVERWRITE_WARNING = "WARNING!!! There is a file under the same name as the destination of conversion." \
+                    "The conversion process will overwrite this file.\nProceed? (y/n)\n"
+
+
+def does_file_exist(filename):
+    return os.path.isfile(filename) or os.path.isfile("LaTeX_Outputs/"+filename)
+
+
+def reader(filename):
+    """Raises IOError if filename does not exist"""
+    file = open(filename, 'r')
+
+    corpus = ""
+    for line in file:
+        corpus += line
+
+    file.close()
+    return corpus
+
+
+def writer(filename, content):
+    """WILL OVERWRITE ANY EXISTING FILE!"""
+    file = open("LaTeX_Outputs/"+filename, 'w')
+    file.write(content)
+    file.close()
+
+
+def starting_process():
+    try:
+        assert len(sys.argv) >= 2
+        if not does_file_exist("base_text.tex"):  # I'm bad, so this is how I handled refactoring this all
+            raise IOError
+    except AssertionError:
+        print(NOT_ENOUGH_ARGS_ERROR_MESSAGE)
+        return False
+    except IOError:
+        print(BASE_TEXT_NOT_FOUND_ERROR_MESSAGE)
+        return False
+
+    print(RUNNING_CONVERSION_MESSAGE)
+
+    if not os.path.exists("LaTeX_Outputs"):  # verify output directory
+        print(MAKING_OUT_DIR_MESSAGE)
+        os.makedirs("LaTeX_Outputs")
+
+    return True
+
+
+def converter(input_file_path):
+    base = reader("base_text.tex")\
+        .replace("replace_with_title", input_file_path.split("/")[-1].split(".")[0].upper())\
+        .replace("replace_with_date", time.strftime("%d/%m/%Y"))
+    new = reader(input_file_path)
+
+    arr = new.split('\n')
+    out_arr = list()
+
+    if len(arr[0]) < 20:
+        out_arr.append("\section*{%s}" % arr[0])
+    else:
+        out_arr.append(arr[0])
+
+    for i in range(1, len(arr)):
+        if arr[i-1] == "" and len(arr[i]) < 20:
+            make_heading = 1
+
+
+    return base + new + END_DOC_STR
+
+
+def main():
+    if not starting_process():
+        return
+
+    args = list(set(sys.argv[1:]))  # actual filepath arguments, removes repeats and the script name
+
+    completed_list = list()
+
+    for filepath in args:
+
+        if not does_file_exist(filepath):
+            print(IOERROR_MESSAGE + "\n" + BAD_FILE_PATH_MESSAGE)
+            continue
+
+        file_name = str(filepath).split("/")[-1]
+        print("Converting ", file_name, "...")
+
+        corpus = converter(file_name)
+        print(SUCCESS_MESSAGE)
+
+        new_file_name = file_name.split(".")[0] + ".tex"
+        if does_file_exist(new_file_name):
+            response = input(OVERWRITE_WARNING)
+            if response == "y" or response == "Y":
+                print("Overwritting", new_file_name)
+            else:
+                print("Skipping", new_file_name)
+                continue
+
+        writer(new_file_name, corpus)
+        print()
+        completed_list.append(new_file_name)
+
+
+
+
+    # finish program and print converted files
+    print(COMPLETED_MESSAGE)
+    [print("LaTeX_Outputs/"+elem) for elem in completed_list]
+    print("~~~\n")
+
+
+
+
+
+main()
